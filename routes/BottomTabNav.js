359673de522df75789fe8a3bcfe69441f1012124
Shompoth KHAN <dadoulan@gmail.com>
2021-12-20 14:28:54 +0100

    icons on bottom tab nav

--- routes/BottomTabNav.js
+++ routes/BottomTabNav.js
@@ -8,16 +8,67 @@ import More from "../screens/More";
 import Squad from "../screens/Squad";
 import Challenges from "../screens/Challenges";
 
+//Styles
+import globalStyles from "../styles/globalStyles";
+
+//Icon
+import { MaterialIcons } from "@expo/vector-icons";
+
 const BottomTab = createBottomTabNavigator();
 
 export const BottomNavigator = () => {
     return (
-        <BottomTab.Navigator>
-            <BottomTab.Screen name="Magazine" component={Magazine} />
-            <BottomTab.Screen name="Challenges" component={Dashboard} />
-            <BottomTab.Screen name="Dashboard" component={More} />
-            <BottomTab.Screen name="Équipes" component={Squad} />
-            <BottomTab.Screen name="Plus" component={Challenges} />
+        <BottomTab.Navigator
+            initialRouteName="Dashboard"
+            screenOptions={{
+                tabBarActiveTintColor: globalStyles.primary,
+            }}
+        >
+            <BottomTab.Screen
+                name="Magazine"
+                component={Magazine}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="smartphone" size={size} color={color} />
+                    ),
+                }}
+            />
+            <BottomTab.Screen
+                name="Challenges"
+                component={Challenges}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="flag" size={size} color={color} />
+                    ),
+                }}
+            />
+            <BottomTab.Screen
+                name="Dashboard"
+                component={Dashboard}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="bolt" size={size} color={color} />
+                    ),
+                }}
+            />
+            <BottomTab.Screen
+                name="Équipes"
+                component={Squad}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="groups" size={size} color={color} />
+                    ),
+                }}
+            />
+            <BottomTab.Screen
+                name="Plus"
+                component={More}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialIcons name="more-horiz" size={size} color={color} />
+                    ),
+                }}
+            />
         </BottomTab.Navigator>
     );
 };
---
+++ styles/globalStyles.js
@@ -0,0 +1,6 @@
+export default {
+    primary: "#6e56e3",
+    white: "white",
+    lightGrey: "#ccc",
+    darkGrey: "#686868",
+};
